a853dbc0ef110ac9bd6e49c327c8fa76071f2d4d
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-06-18 17:37:49 +0300

    update business class methods

--- trends/models.py
+++ trends/models.py
@@ -60,9 +60,15 @@ class N_Hood(models.Model):
 class Business(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    email=models.EmailField(null=True)
+    n_hood=models.ForeignKey(N_Hood,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
+    def create_business(self):
+        self.create()
 
     def save_business(self):
         self.save()
