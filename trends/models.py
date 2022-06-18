ca67361d89e2d0de9020bace3b638aebd3c8880c
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-06-18 17:35:37 +0300

    update user class methods

--- trends/models.py
+++ trends/models.py
@@ -78,8 +78,10 @@ class Business(models.Model):
 
 
 class User_Business(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    business = models.ForeignKey(Business, on_delete=models.CASCADE)
+    name=models.CharField(max_length=50)
+    id=models.AutoField(primary_key=True)
+    n_hood=models.ForeignKey(N_Hood, on_delete=models.CASCADE)
+    email=models.EmailField(null=True)
 
     def __str__(self):
         return self.user.username
