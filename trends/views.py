b5528d42c98c7e8d2a7161c7d4b9357844c6779b
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-06-18 12:20:10 +0300

    add signup views in views.py file

--- trends/views.py
+++ trends/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from trends.forms import RegisterForm
 
 # Create your views here.
 
+def home(request):
+    return render(request, 'home.html')
 
+
+def signup(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()   
+
+        return redirect('login')
+    else:    
+        form = RegisterForm()
+    return render(request,'registration/signup.html',{'form':form})
 
 
