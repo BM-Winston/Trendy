15242161244e75598a0dc69b371ea89beb859533
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-06-18 17:41:02 +0300

    register models in admin

--- trends/admin.py
+++ trends/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Profile
+from .models import Profile, User, Business, N_Hood
 
 # Register your models here.
 admin.site.register(Profile)
+admin.site.register(User)
+admin.site.register(Business)
+admin.site.register(N_Hood)
