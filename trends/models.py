a54c985461672a4432db6ad79540472e372a9bf9
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-06-18 16:36:45 +0300

    add profile model functions

--- trends/models.py
+++ trends/models.py
@@ -12,3 +12,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def update_profile(self):
+        self.update()
