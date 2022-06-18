67aed86df7494cd9f25602e72a71e114e8795a34
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-06-18 12:26:45 +0300

    add sign up and home url patterns in app urls.py

---
+++ trends/urls.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from . import views
+from django.urls import path, include
+from django.conf.urls.static import static
+
+urlpatterns=[
+    path('',views.home,name = 'home'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+]
