4a4ebe52c6d2941a98ac11a13d77cbd750ad68bf
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-06-18 12:18:12 +0300

    add form.py file adn registerform

---
+++ trends/forms.py
@@ -0,0 +1,14 @@
+from dataclasses import fields
+import email
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from .models import *
+
+
+class RegisterForm(UserCreationForm):
+    email=forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username', 'email','password1', 'password2']
