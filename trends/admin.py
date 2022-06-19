d8a02db2c0f1b297881d5294bb1830ec6e03e3b2
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-06-19 23:56:11 +0300

    add post model

--- trends/admin.py
+++ trends/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
-from .models import Profile, User, Business, N_Hood
+from .models import Profile, Business, N_Hood
 
 # Register your models here.
 admin.site.register(Profile)
-admin.site.register(User)
 admin.site.register(Business)
 admin.site.register(N_Hood)
--- trends/models.py
+++ trends/models.py
@@ -27,9 +27,11 @@ class Profile(models.Model):
         self.update()
 
 class N_Hood(models.Model):
+    n_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=50)
     occupants = models.IntegerField()
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
     
 
     def __str__(self):
@@ -58,6 +60,7 @@ class N_Hood(models.Model):
 
 
 class Business(models.Model):
+    n_hood = models.ForeignKey(N_Hood, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
     user=models.ForeignKey(User,on_delete=models.CASCADE)
@@ -100,5 +103,32 @@ class User_Business(models.Model):
 
     def update_user_business(self):
         self.update()
+
+
+class Post(models.Model):
+    n_hood = models.ForeignKey(N_Hood, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    post = models.TextField()
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    email=models.EmailField(null=True)
+    
+
+    def __str__(self):
+        return self.title
+
+    def create_post(self):
+        self.create()
+
+    def save_post(self):
+        self.save()
+
+    def delete_post(self):
+        self.delete()
+
+    def update_post(self):
+        self.update()
+
+    def search_post(self):
+        return Post.objects.filter(title__icontains=self.title)
 
     
